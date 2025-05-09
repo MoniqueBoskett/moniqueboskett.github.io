322958df925d6b05e345af04adc3c7dbaf80b4b1
Monique Boskett <monique.boskett@gmail.com>
2025-05-09 08:15:27 -0400

    Add and apply Fira Sans font site-wide

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        {/* Google Fonts: Fira Sans */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
